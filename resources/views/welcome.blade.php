eedf98772cfbfb90fb6db3f724e712f58d35a587
Aditio <aditio.pluto@gmail.com>
2025-06-03 01:11:55 +0700

    update animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,38 @@
         html {
             font-family: 'Onest', Arial, Helvetica, sans-serif;
         }
+
+        .fade-in {
+            animation: fadeIn 1s ease-out both;
+        }
+
+        .slide-down {
+            animation: slideDown 1s ease-out both;
+        }
+
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+                transform: translateY(10px);
+            }
+
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+
+        @keyframes slideDown {
+            from {
+                opacity: 0;
+                transform: translateY(-20px);
+            }
+
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
     </style>
 </head>
 
@@ -24,21 +56,21 @@
         <img src="{{ asset('images/logo-rw-digital.png') }}" alt="RW Digital Logo" class="mx-auto w-32 mb-6">
 
         <!-- Headline -->
-        <h1 class="text-3xl font-medium mb-2">Selamat Datang di RW Digital</h1>
-        <p class="text-slate-400 mb-8 max-w-md mx-auto">
+        <h1 class="text-3xl font-medium mb-2 fade-in">Selamat Datang di RW Digital</h1>
+        <p class="text-slate-400 mb-8 max-w-md mx-auto fade-in">
             Platform digital untuk pengelolaan dan disposisi surat secara efisien dan modern. <br>Dapatkan kemudahan
             mengurus berbagai surat, seperti Surat Domisili, Surat Kematian, SKTM, Surat Izin Nikah, dan Surat Izin
             Kegiatan.
         </p>
 
-        <div class="flex items-center justify-center gap-2">
+        <div class="flex items-center justify-center gap-2 fade-in">
             <!-- Login Button -->
             <a href="{{ route('filament.panel.pages.dashboard') }}"
                 class="inline-block px-6 py-3 text-sm font-medium rounded-xl bg-cyan-500 hover:bg-cyan-600 transition">
                 Masuk ke Aplikasi
             </a>
             <!-- Register Button -->
-            <a href="{{ route('filament.panel.pages.dashboard') }}"
+            <a href="{{ route('filament.panel.pages.dashboard') }} fade-in"
                 class="inline-block px-6 py-3 text-sm font-medium rounded-xl bg-cyan-500 hover:bg-cyan-600 transition">
                 Daftar Sekarang
             </a>
