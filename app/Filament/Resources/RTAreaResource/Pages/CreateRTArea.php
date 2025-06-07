b83c5cc9b3bfa95d00a8a7a3abbe7bb3e0cd8f10
Aditio <aditio.pluto@gmail.com>
2025-06-08 02:21:52 +0700

    Update RT

--- app/Filament/Resources/RTAreaResource/Pages/CreateRTArea.php
+++ app/Filament/Resources/RTAreaResource/Pages/CreateRTArea.php
@@ -3,10 +3,79 @@
 namespace App\Filament\Resources\RTAreaResource\Pages;
 
 use App\Filament\Resources\RTAreaResource;
+use Dotswan\MapPicker\Fields\Map;
 use Filament\Actions;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRTArea extends CreateRecord
 {
     protected static string $resource = RTAreaResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            Section::make('Informasi Rukun Tetangga')
+                ->description('Masukkan informasi mengenai nama dan lokasi rukun tetangga')
+                ->schema([
+                    TextInput::make('name')
+                        ->label('Nama RT')
+                        ->unique()
+                        ->required(),
+
+                    Grid::make()->columnSpan(1),
+
+                    Textarea::make('address')
+                        ->label('Alamat Lengkap')
+                        ->rows(6)
+                        ->required(),
+
+                    Grid::make()->columnSpan(1),
+
+                    Hidden::make('latitude'),
+
+                    Hidden::make('longitude'),
+
+                    Hidden::make('google_maps'),
+
+                    Map::make('location')
+                        ->label('Maps')
+                        ->defaultLocation(latitude: -6.205979, longitude: 106.756135)
+                        ->showMarker()
+                        ->markerColor("#004A7C")
+                        ->markerIconAnchor([18, 36])
+                        ->markerIconSize([36, 36])
+                        ->columnSpanFull()
+                        ->zoom(18)
+                        ->draggable()
+                        ->extraStyles([
+                            'min-height: 40vh',
+                            'border-radius: 5px'
+                        ])
+                        ->afterStateUpdated(function ($set, ?array $state): void {
+                            $set('latitude', $state['lat']);
+                            $set('longitude', $state['lng']);
+                            $set('google_maps', 'https://www.openstreetmap.org/#map=18/' . $state['lat'] . '/' . $state['lng']);
+                        })
+                ])
+                ->columns(2)
+        ]);
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        unset($data['location']);
+
+        return $data;
+    }
 }
--- app/Filament/Resources/RTAreaResource/Pages/EditRTArea.php
+++ app/Filament/Resources/RTAreaResource/Pages/EditRTArea.php
@@ -3,7 +3,14 @@
 namespace App\Filament\Resources\RTAreaResource\Pages;
 
 use App\Filament\Resources\RTAreaResource;
+use Dotswan\MapPicker\Fields\Map;
 use Filament\Actions;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRTArea extends EditRecord
@@ -15,5 +22,73 @@ class EditRTArea extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            Section::make('Informasi Rukun Tetangga')
+                ->description('Masukkan informasi mengenai nama dan lokasi rukun tetangga')
+                ->schema([
+                    TextInput::make('name')
+                        ->label('Nama RT')
+                        ->unique(ignoreRecord: true)
+                        ->required(),
+
+                    Grid::make()->columnSpan(1),
+
+                    Textarea::make('address')
+                        ->label('Alamat Lengkap')
+                        ->rows(6)
+                        ->required(),
+
+                    Grid::make()->columnSpan(1),
+
+                    Hidden::make('latitude'),
+
+                    Hidden::make('longitude'),
+
+                    Hidden::make('google_maps'),
+
+                    Map::make('location')
+                        ->label('Maps')
+                        ->defaultLocation(latitude: -6.205979, longitude: 106.756135)
+                        ->showMarker()
+                        ->markerColor("#004A7C")
+                        ->markerIconAnchor([18, 36])
+                        ->markerIconSize([36, 36])
+                        ->columnSpanFull()
+                        ->zoom(18)
+                        ->draggable()
+                        ->extraStyles([
+                            'min-height: 40vh',
+                            'border-radius: 5px'
+                        ])
+                        ->afterStateUpdated(function ($set, ?array $state): void {
+                            $set('latitude', $state['lat']);
+                            $set('longitude', $state['lng']);
+                            $set('google_maps', 'https://www.openstreetmap.org/#map=18/' . $state['lat'] . '/' . $state['lng']);
+                        })
+                        ->afterStateHydrated(function ($set, $record): void {
+                            $set('location', [
+                                'lat' => $record->latitude,
+                                'lng' => $record->longitude,
+                            ]);
+                        })
+                ])
+                ->columns(2)
+        ]);
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        unset($data['location']);
+
+        return $data;
     }
 }
